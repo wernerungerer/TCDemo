4421ae32ee2c7b40ca5d08884d4f941a285dfab1
Werner Ungerer <werner.ungerer@gmail.com>
2013-09-29 17:16:08 +0100

    IC

---
+++ TCDemo/TCDemo.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using TCDemo;
+using TCDemo.Controllers;
+
+namespace TCDemo.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+        }
+
+       [Test]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        [Category("longrunning")]
+        public void LongRunningConsole()
+        {
+            long sum = 0;
+            var length = 100000;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    for (int k = 0; k < length; k++)
+                    {
+                        var number = i * k * j;
+                        sum += number;
+                    }
+                }
+            }
+
+            Console.WriteLine("Number value is" + sum);
+
+        }
+    }
+}
